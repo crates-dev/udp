35d34df97e104cf195cedd3e4d8df1dc392fd33c
尤雨东 <root@ltpp.vip>
2025-03-03 08:41:17 +0800

    feat: v0.6.0

--- src/server/socket/type.rs
+++ src/server/socket/type.rs
@@ -11,9 +11,9 @@ pub type ArcRwLockWriteGuardUdpSocket<'a> = Arc<RwLockWriteGuard<'a, UdpSocket>>
 pub type OptionArcRwLockWriteGuardUdpSocket<'a> = Option<ArcRwLockWriteGuardUdpSocket<'a>>;
 pub type ArcMutexGuardUdpSocket<'a> = Arc<MutexGuard<'a, UdpSocket>>;
 pub type OptionArcMutexGuardUdpSocket<'a> = Option<ArcMutexGuardUdpSocket<'a>>;
+pub type OptionClientHost = Option<IpAddr>;
+pub type OptionClientPort = Option<u16>;
 pub type OptionSocketAddr = Option<SocketAddr>;
-pub type ClientHost = IpAddr;
-pub type ClientPort =  u16;
 
 #[derive(Clone, Debug)]
 pub struct ArcRwLockUdpSocket(pub(super) ArcRwLock<UdpSocket>);
