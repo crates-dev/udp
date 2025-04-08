81e2bf6441b8b2f4f141baadde6215f6d105680a
尤雨东 <root@ltpp.vip>
2025-04-08 19:24:03 +0800

    feat: v0.19.0

--- src/utils/error.rs
+++ src/utils/error.rs
@@ -1,5 +1,5 @@
 use crate::*;
 
-pub fn handle_error(tmp: &Tmp, err_string: &str) {
+pub fn handle_error<T: ToString>(tmp: &Tmp, err_string: T) {
     tmp.get_log().error(err_string, common_log);
 }
